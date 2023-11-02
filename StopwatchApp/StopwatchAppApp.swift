5ec4d58fc3e77a9e436aa0dfeae1dbd3f6af9ed3
Dexter Ng <DNg@lcs.on.ca>
2023-11-02 08:45:09 -0400

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Dexter Ng on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
