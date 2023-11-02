bec87c4a7c7c5fd3d5c7473b4c987937f29d0ca9
Dexter Ng <DNg@lcs.on.ca>
2023-11-02 09:27:49 -0400

    created CircleButtonView as a helper to make ContentView shorter.

---
+++ StopwatchApp/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  StopwatchApp
+//
+//  Created by Dexter Ng on 2023-11-02.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
