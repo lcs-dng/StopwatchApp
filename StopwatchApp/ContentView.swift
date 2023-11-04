1f077cc39c75e0d9e265043947f093c172126a59
Dexter Ng <DNg@lcs.on.ca>
2023-11-04 10:31:31 -0400

    made the selected tab item default to the Stopwatch.

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -51,31 +51,35 @@ struct ContentView: View {
 
 #Preview {
     
-    TabView {
+    TabView(selection: Binding.constant(3)) {
         
         Text("World Clock")
             .tabItem {
                 Image(systemName: "globe")
                 Text("World Clock")
             }
+            .tag(1)
         
         Text("Alarm")
             .tabItem {
                 Image(systemName: "alarm.fill")
                 Text("Alarm")
             }
+            .tag(2)
         
         ContentView()
             .tabItem {
                 Image(systemName: "stopwatch.fill")
                 Text("Stopwatch")
             }
+            .tag(3)
         
         Text("Timer")
             .tabItem {
                 Image(systemName: "timer")
                 Text("Timer")
             }
+            .tag(4)
         
     }
     // Change the accent colour for the currently active tab item
